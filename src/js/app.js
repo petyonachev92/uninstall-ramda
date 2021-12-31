66fed2ddd8164711f04135a2fc4a45fa043cba19
Petyo Nachev <pyo@abv.bg>
2021-12-31 19:16:34 +0200

    fix: removed ramda

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 import "../scss/app.scss";
-const R = require("ramda");
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
   const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  const filteredArray = numbersArray.filter(isEven);
+
+  filteredArray.forEach(element => {
+    let li = document.createElement('li');
+    li.textContent = element;
+
+    ul.appendChild(li);
+  })
 });
